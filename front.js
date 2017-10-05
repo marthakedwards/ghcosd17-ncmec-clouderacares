bfad826db4c67469d6471bc73a9a4e82484a04d9
Martha Edwards <martha_edwards@brown.edu>
2017-10-05 15:04:11 -0400

    Add map icons and dropdowns

--- front.js
+++ front.js
@@ -8,30 +8,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
     zoom: 3
   });
 
+  // data scheme we are using
+  var dataSet = [
+    {
+      location: {
+        state: "Florida",
+        postalCode: "32836",
+        city: "Orlando",
+        county: "",
+        coordinates: [-81.483570, 28.426323]
+      },
+      abductor: {
+        race: "White",
+        gender: "Male",
+        age: "25",
+        lure: ""
+      },
+      vehicle: {
+        type: "Van",
+        color: "Black"
+      },
+      abductee: {
+        gender: "Female",
+        age: "6",
+        race: "Hispanic"
+      },
+      date: "01/02/16",
+      time: "9:00 PM"
+    },
+  ]
+
+  var features = [];
+
+  dataSet.forEach(function(element) {
+    features.push({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: element.location.coordinates
+      },
+      properties: element
+    })
+  });
+
   var geojson = {
     type: 'FeatureCollection',
-    features: [{
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-77.032, 38.913]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'Washington, D.C.'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [-122.414, 37.776]
-      },
-      properties: {
-        title: 'Mapbox',
-        description: 'San Francisco, California'
-      }
-    }]
+    features: features
   };
 
   // add markers to map
@@ -45,9 +67,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     new mapboxgl.Marker(el)
       .setLngLat(marker.geometry.coordinates)
       .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-      .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+      .setHTML(
+        '<h3>Attempted abduction</h3>' +
+        '<p>Time: ' + marker.properties.date + ', ' + marker.properties.time + '</p>' +
+        '<p>Location: ' + marker.properties.location.city + ', ' + marker.properties.location.state + ', ' + marker.properties.location.postalCode + '</p>' +
+        '<p>Abductor information: ' + marker.properties.abductor.race + ', ' + marker.properties.abductor.gender + ', ' + marker.properties.abductor.age + ' years old' + '</p>' +
+        '<p>Vehicle description: ' + marker.properties.vehicle.type + ', ' + marker.properties.vehicle.color + '</p>' + 
+        '<p>Abductee information: ' + marker.properties.abductee.race + ', ' + marker.properties.abductee.gender + ', ' + marker.properties.abductee.age + '</p>'
+      ))
       .addTo(map);
   });
-  
 
 });
